e31d8255589f45eacf4d8d2d5db2c0044f7fe3e0
venkat-netizen <150734192+venkat-netizen@users.noreply.github.com>
2025-07-02 16:45:28 +0200

    Added scripts and GUI folder and Window and WindowManager classes

---
+++ Lecture_Translator_Frontend/Assets/Scripts/GUI/WindowManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WindowManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

b046668c839e0666e1aa90da3ea5c8c1a11af684
jtnzyxwlm <uyuri@student.kit.edu>
2025-07-08 01:35:34 +0200

    A

---
+++ Lecture_Translator_Frontend/Assets/Scripts/SettingsModule/DisplayModeController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DisplayModeController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

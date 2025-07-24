a1d0bb603129ca646f644bfd6232d000c1d574d5
Manuel Pielka <manuel@pielka.de>
2025-07-24 17:17:25 +0200

    Update Login.cs

--- Lecture_Translator_Frontend/Assets/Scripts/BrowsingModule/Login.cs
+++ Lecture_Translator_Frontend/Assets/Scripts/BrowsingModule/Login.cs
@@ -2,16 +2,23 @@ using UnityEngine;
 using UnityEngine.Networking;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public static class Login
 {
-    private const string URL_REGEX = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$";
+    private const string URL_REGEX = @"/dex/auth/shib\?client_id=[\S]+&amp;redirect_uri=[\S]+&amp;response_type=code&amp;scope=[\S]+&amp;state=[\S]+";
     private const string REQUEST_TOKEN_URL = "https://lecture-translator.kit.edu/gettoken";
+    private const string SERVER_URL = "https://lecture-translator.kit.edu";
 
     public static async Task<string> GetToken(string username, string password)
     {
         string loginOptionPage = await PostRequest(REQUEST_TOKEN_URL);
-        
+        Debug.Log(loginOptionPage);
+        MatchCollection options = Regex.Matches(loginOptionPage, URL_REGEX);
+        Debug.Log(SERVER_URL + options[0].Value);
+        string shibbolethPage = await GetRequest(SERVER_URL + options[0].Value);
+        Debug.Log(shibbolethPage);
+
 
         return null;
     }
@@ -19,6 +26,25 @@ public static class Login
     private static async Task<string> PostRequest(string url)
     {
         UnityWebRequest request = new UnityWebRequest(url, "POST");
+        request.uploadHandler = new UploadHandlerRaw(null);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        await request.SendWebRequest();
+
+        Dictionary<string, string> headers = request.GetResponseHeaders();
+        foreach (string headercode in headers.Keys)
+        {
+            Debug.Log(headercode + ": " + headers[headercode]);
+        }
+
+
+        return request.downloadHandler.text;
+
+    }
+    
+    private static async Task<string> GetRequest(string url)
+    {
+        UnityWebRequest request = new UnityWebRequest(url, "GET");
         request.uploadHandler = new UploadHandlerRaw(null);
         request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
